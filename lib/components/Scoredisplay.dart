49e5f5f265274a7fa9e1e00b9f77515b75654a5c
ProgrammerID23 <76148355+ProgrammerID23@users.noreply.github.com>
2024-01-01 20:59:36 +0700

    first commit

---
+++ lib/components/Scoredisplay.dart
@@ -0,0 +1,30 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class ScoreComponent extends PositionComponent {
+  late TextPainter textPainter;
+  late TextStyle textStyle;
+  late int score;
+
+  ScoreComponent(this.score) {
+    textStyle = TextStyle(color: Colors.white, fontSize: 24);
+    textPainter = TextPainter(
+      text: TextSpan(text: 'Score: $score', style: textStyle),
+      textDirection: TextDirection.ltr,
+    )..layout();
+  }
+
+  void updateScore(int newScore) {
+    score = newScore;
+    textPainter.text = TextSpan(text: 'Score: $score', style: textStyle);
+    textPainter.layout();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    textPainter.paint(canvas, Offset(20, 20));
+  }
+
+  @override
+  void update(double dt) {}
+}
